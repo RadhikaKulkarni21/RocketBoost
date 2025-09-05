44726486f20c6004ebb3aaba5040fb0e207b7ed7
radhi <radhika2103@gmail.com>
2025-09-05 11:25:57 +0100

    thurst and rotation movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,20 +1,43 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
     [SerializeField] InputAction thrust;
+    [SerializeField] InputAction rotation;
+    [SerializeField] float thrustStrength = 100f;
+
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     void OnEnable()
     {
         thrust.Enable();
+        rotation.Enable();
     }
 
-    void Update()
+    void FixedUpdate()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    private void ProcessRotation()
+    {
+        float rotationInput = rotation.ReadValue<float>();
+        Debug.Log("Rotation value: " + rotationInput);
+    }
+
+    private void ProcessThrust()
     {
         if (thrust.IsPressed())
         {
-            Debug.Log("I believe I can fly");
+            rb.AddRelativeForce(Vector3.up * thrustStrength * Time.fixedDeltaTime);
         }
     }
 }
